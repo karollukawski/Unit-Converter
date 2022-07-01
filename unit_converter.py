0a80eb8567a4ee70ea56d6700cadd27c2851b1d3
karollukawski <83611706+karollukawski@users.noreply.github.com>
2022-07-01 12:00:25 +0200

    Update v1

--- unit_converter.py
+++ unit_converter.py
@@ -1,7 +1,46 @@
+choice = -1
+
+def get_lenght():
+    lenght = int(input("Give the number of your lenght: "))
+    
+    print ()
+    print ("1. Kilometers")
+    print ("2. Meters")
+    print ("3. Decymeters")
+    print ("4. Centimeters")
+    print ("5. Milimeters")
+    print ("6. Yards")
+    print ("7. Feet")
+    print ("8. Inches")
+    print ("9. Miles")
+    print ("10. Sea miles")
+
+    lenght_unit = int(input("Give the number of your unit: "))
+
 while True:
     print ()
     print ("0. Lenght")
     print ("1. Weight")
     print ("2. Time")
     print ("3. Area")
+    print ("4. Volume")
+    print ("5. Temperature")
+    print ("6. Exit")
     print ()
+
+    choice = int(input("Choose your option: "))
+
+    if choice == 0:
+        get_lenght()
+    if choice == 0:
+        print ()
+    if choice == 0:
+        print ()
+    if choice == 0:
+        print ()
+    if choice == 0:
+        print ()
+    if choice == 0:
+        print ()
+    if choice == 0:
+        break    
